1800a7a0e27fbab2fed393855a6efc9fdc1d89a2
maruf-imtiaz <marufimtiaz34@gmail.com>
2025-07-04 09:37:18 +0600

    Chapter-1.js

--- Chapter-1.js
+++ Chapter-1.js
@@ -1,21 +1,23 @@
-//Chapter 1 - Create a variable type string and try to add a number to it
+//Chapter 1 
+ 
+// Create a variable type string and try to add a number to it
 let a= "Maurf" //string variable
 let b= "34"
-console.log(a+b);
-//Chapter 1 - Use type of operator to find the data type of the string and number variable
+console.log(a+b); //output will be Maurf34 because it concatenates the string and number variables
+//Use type of operator to find the data type of the string and number variable
 console.log(typeof a+b); //output will be string34
 console.log(typeof (a+b)); //output will be string
-//Chapter 1 - Create a const object in JavaScript. Can you change it to hold a number later?
+//Create a const object in JavaScript. Can you change it to hold a number later?
 const c={
     name: "Jubair",
     age: 22              //Now if I type let c= "Maruf"; it will throw an error because const cannot be reassigned. 
 }                        //So the answer will be no, you cannot change a const object to hold a number later.
 console.log(c);          //output will be { name: 'Jubair', age: 22 }
-//Chapter 1 - Try to add a new key in the const object. Were you able to do it?
+//Try to add a new key in the const object. Were you able to do it?
 c['name'] = "Maruf"; //Yes, you can add a new key in the const object.
 c['age'] = 23;       //Yes, you can also change the value of the existing key in the const object.
 console.log(c);      //output will be { name: 'Maruf', age: 23 }
-//Chapter 1 - Write a JavaScript program to create a word meaning dictionary of 5 words.
+//Write a JavaScript program to create a word meaning dictionary of 5 words.
 const dictionary = {
 Persistence: "The quality of continuing steadily despite problems or difficulties.",
 Focus: "Concentration on a particular task or goal.",
